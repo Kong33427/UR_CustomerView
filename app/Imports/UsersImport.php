ce780a6c8968c1395725369f54ae0e829a6c7d47
KongsinRoj <127832290+KongsinRoj@users.noreply.github.com>
2023-06-12 08:59:42 +0700

    Update UsersImport.php

--- app/Imports/UsersImport.php
+++ app/Imports/UsersImport.php
@@ -16,53 +16,91 @@ class UsersImport implements ToModel, WithHeadingRow
     * @return \Illuminate\Database\Eloquent\Model|null
     */
     private $additionalValue;
+    protected $count;
 
-    public function __construct($additionalValue)
+    public function __construct($additionalValue,$count)
     {
         $this->additionalValue = $additionalValue;
+        $this->count = $count;
     }
     public function model(array $row)
     {
         // var_dump($row);
         return new Clickup([
-            'TASK_ID' => $row['task_id'],
-            'TASK_NAME' => $row['task_name'],
-            'REF_NUM' => $row['reference_numbers_short_text'],
-            'COMPLETION' => $row['completed_manual_progress'],
-            'PROGRESS' => $row['progress_drop_down'],
-            'PHASE' => $row['phase_project_drop_down'],
-            'STATUS' => $row['status'],
-            'BU' => $row['bu_labels'],
-            'TEAM' => $row['team_labels'],
-            'START_DATE' => $row['start_date'],
-            'END_DATE' => $row['due_date'],
-            'BASE_DATE' => $row['baseline_date_date'],
-            'PROJECT_DOC' => $row['project_documents_labels'],
-            'ASIGNEE' => $row['assignee'],
-            'PM_BA_LEADER' => $row['pmbaleader_labels'],
-            'PIC_CIT' => $row['pic_cit_labels'],
-            'REQUESTER' => $row['requester_short_text'],
-            'SPONSOR' => $row['sponsor_short_text'],
-            'VENDOR_OTHER' => $row['vendorother_short_text'],
-            'BUS_FUNCTION' => $row['business_function_labels'],
-            'IMPACT_FALCON' => $row['impact_falcon_drop_down'],
-            'IT_OT' => $row['itot_labels'],
-            'EST_BUDGET' => $row['est_budget_currency'],
-            'LOG_PAY_REQ' => $row['log_request_for_payment_currency'],
-            'RESOURCES' => $row['resouces_drop_down'],
-            'TYPE' => $row['type_labels'],
-            'PRIORITY' => $row['priority'],
-            'REGISTER_YEAR' => $row['register_year_short_text'],
-            'REQ_DATE' => $row['request_date_date'],
-            'LAST_STATUS' => $row['last_status_text'],
-            'CURRENT_STATUS' => $row['current_status_text'],
-            'NEXT_STATUS' => $row['next_status_text'],
-            'MAN_DAY' => $row['manday_number'],
+            'TASK_ID' => $row[0],
+            'TASK_NAME' => $row[1],
+            'REF_NUM' => $row[2],
+            'COMPLETION' => $row[3],
+            'PROGRESS' => $row[4],
+            'PHASE' => $row['5'],
+            'STATUS' => $row['6'],
+            'BU' => $row[7],
+            'TEAM' => $row[8],
+            'START_DATE' => $row[9],
+            'END_DATE' => $row[10],
+            'BASE_DATE' => $row[11],
+            'PROJECT_DOC' => $row[12],
+            'ASIGNEE' => $row[13],
+            'PM_BA_LEADER' => $row[14],
+            'PIC_CIT' => $row[15],
+            'REQUESTER' => $row[16],
+            'SPONSOR' => $row[17],
+            'VENDOR_OTHER' => $row[18],
+            'BUS_FUNCTION' => $row[19],
+            'IMPACT_FALCON' => $row[20],
+            'IT_OT' => $row[21],
+            'EST_BUDGET' => $row[22],
+            'LOG_PAY_REQ' => $row[23],
+            'RESOURCES' => $row[24],
+            'TYPE' => $row[25],
+            'PRIORITY' => $row[26],
+            'REGISTER_YEAR' => $row[27],
+            'REQ_DATE' => $row[28],
+            'LAST_STATUS' => $row[29],
+            'CURRENT_STATUS' => $row[30],
+            'NEXT_STATUS' => $row[31],
+            'MAN_DAY' => $row[32],
             'CREATE_DATE'=> $this->additionalValue,
         ]);
     }
-    public function headingRow(): int
+    public function rules(): array
     {
-        return 3;
+        // Define your validation rules here
+        return [
+            0 => 'required', // Task ID validation
+            1 => 'required', // Task Name validation
+            2 => 'required', // Task Name validation
+            3 => 'required', // Task Name validation
+            4 => 'required', // Task Name validation
+            5 => 'required', // Task Name validation
+            6 => 'required', // Task Name validation
+            7 => 'required', // Task Name validation
+            8 => 'required', // Task Name validation
+            9 => 'required', // Task Name validation
+            10 => 'required', // Task Name validation
+            11=> 'required', // Task Name validation
+            12 => 'required', // Task Name validation
+            13 => 'required', // Task Name validation
+            14 => 'required', // Task Name validation
+            15 => 'required', // Task Name validation
+            16 => 'required', // Task Name validation
+            17 => 'required', // Task Name validation
+            18=> 'required', // Task Name validation
+            19 => 'required', // Task Name validation
+            20 => 'required', // Task Name validation
+            21 => 'required', // Task Name validation
+            22 => 'required', // Task Name validation
+            23=> 'required', // Task Name validation
+            24=> 'required', // Task Name validation
+            25=> 'required', // Task Name validation
+            26=> 'required', // Task Name validation
+            27=> 'required', // Task Name validation
+            28=> 'required', // Task Name validation
+            29=> 'required', // Task Name validation
+            30=> 'required', // Task Name validation
+            31=> 'required', // Task Name validation
+            32=> 'required', // Task Name validation
+            // Rest of the fields' validation rules
+        ];
     }
 }
